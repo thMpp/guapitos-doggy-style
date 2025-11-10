75b45bcf61255cbce9f33c1febd676884e660468
thMpp <pmatias125@gmail.com>
2025-11-10 15:53:26 -0300

    ola

--- BD/src/server.ts
+++ BD/src/server.ts
@@ -3,13 +3,18 @@ import cors from "cors";
 import { PrismaClient } from "@prisma/client";
 import dotenv from "dotenv";
 
+
 dotenv.config();
 
 const app = express();
 const prisma = new PrismaClient();
-
 app.use(cors());
 app.use(express.json());
+app.use(cors({
+  origin: process.env.CORS_ORIGIN?.split(",") || "*"
+}));
+app.get("/health", (_req, res) => res.json({ok:true}));
+
 
 // Logger simple
 app.use((req, _res, next) => {
